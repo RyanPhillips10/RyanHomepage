f402aad0401a4c6b675d876700d789b60145fbf6
Coding <coding@Ryans-MacBook-Pro-2.local>
2016-09-29 23:08:12 -0700

    Added about page

--- test/test.js
+++ test/test.js
@@ -34,3 +34,16 @@ describe('Main Page', function () {
 		});
 	});
 });
+
+describe('About Page', function () {
+	describe('About Page has started', function () {
+		it ('About Page has started', function  () {
+			chai.request(server)
+            .get('/about')
+            .end((err, res) => {
+                res.should.have.status(200);
+            	done();
+            });
+		});
+	});
+});
